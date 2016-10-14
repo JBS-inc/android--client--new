78c6501b9884f36dab65beeccf48ea04c8f92a90
Jack Horsburgh <jackhorse1000@gmail.com>
2016-10-14 23:07:33 +0100

    added QR button                       added score                       added list view                       hard coded some values
     2. added qr scanner
     3.linked login button to achievements page

--- app/src/main/java/com/example/jack/mylibrary/AchievementsActivity.java
+++ app/src/main/java/com/example/jack/mylibrary/AchievementsActivity.java
@@ -1,18 +1,25 @@
 package com.example.jack.mylibrary;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
-public class AchievementsActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class AchievementsActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievements);
-        Intent intent = getIntent();
 
 
         String[] achievements = {"Score","Goal","sufuhgh","jdfhkhdfk","hsdfgsdhf","husdihfuih"};
@@ -21,4 +28,29 @@ public class AchievementsActivity extends AppCompatActivity {
         ListView achievementListView = (ListView) findViewById(R.id.achievements_list_view);
         achievementListView.setAdapter(CustomAdapter);
     }
+
+    public void onScan(View view){
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    protected void onPause(){
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    public void handleResult(Result result){
+        //Do something with our result here
+
+        Log.v("handleResult", result.getText());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setMessage(result.getText());
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
 }
